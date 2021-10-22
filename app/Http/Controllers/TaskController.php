6e1bac509e6af95c6a869cf8e721f01e98577f36
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-10-22 18:31:40 -0400

    creating task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json(['tasks'=> $tasks], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $task = new Task();
+        $task->name = $request->name;
+        $task->completed = $request->completed;
+
+        $task->save();
+
+        return response()->json(['task'=> $task], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        $task = Task::findOrFail($request->id);
+        // error_log(json_encode($task));
+
+        return response()->json(['task'=> $task], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $task = Task::findOrFail($request->id);
+        $task->name = $request->name;
+        $task->completed = $request->completed;
+
+        $task->save();
+
+        return response()->json(['task'=> $task], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $task = Task::destroy($request->id);
+        return response()->json(['task'=> $task], 200);
+    }
+}
